d432688d839d183c62564129c6c9a428cc09cd66
Artemeva Marina <rinko-69@yandex.ru>
2022-10-09 12:18:41 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,8 +1,8 @@
 import os
 from datetime import timedelta
+from pathlib import Path
 
 from dotenv import load_dotenv
-from pathlib import Path
 
 dotenv_path = Path('yamdb_final/infra/.env')
 load_dotenv(dotenv_path=dotenv_path)
