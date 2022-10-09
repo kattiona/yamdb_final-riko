012b034be66d5a077fbb91482e0f228c3e460272
Artemeva Marina <rinko-69@yandex.ru>
2022-10-09 12:07:32 +0300

    Update admin.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Category, Genre, Title, GenreTitle, Review
+from .models import Category, Genre, GenreTitle, Review, Title, User
 
 
 admin.site.register(User)
