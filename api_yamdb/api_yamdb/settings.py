7bdce1c922b8be47015c0ca6961f6c5e283b751c
root <root@LAPTOP-BKCG9FLK.localdomain>
2022-10-31 20:02:27 +0300

    Добавила айпи в настройки

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.2.226', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['158.160.2.226', '127.0.0.1', 'localhost', '51.250.31.16']
 
 
 # Application definition
